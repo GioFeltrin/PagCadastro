81ec79298772280a1a611f4b5720d81059b4a7e2
GioFeltrin <giovanni.affornalli@gmail.com>
2022-12-14 23:11:04 -0300

    meu primeiro commit

---
+++ JS/script.js
@@ -0,0 +1,39 @@
+
+
+
+function clickMenu(){
+    if(superior.style.display == 'block') {
+        superior.style.display = 'none'
+    } else {
+        superior.style.display = 'block'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        superior.style.display = 'block'
+    } else {
+        superior.style.display = 'none'
+    }
+}
+
+
+let quantidade = document.querySelector ('qtd')
+let add = document.querySelector ('.addCar')
+add.addEventListener ('click', 'cart');
+
+function cart() {
+    quantidade ++
+    document.getElementById ('qtd').innerText=quantidade
+}
+
+let rmv = document.querySelector ('.remove')
+rmv.addEventListener ('click', 'remove')
+
+function remove (){
+    quantidade --
+    if (quantidade == 00) 
+        {}
+        document.getElementById ('qtd').innerText=quantidade
+    
+    }
