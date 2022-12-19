5457ac18e288a6aae45d8f16353df74a15487216
GioFeltrin <giovanni.affornalli@gmail.com>
2022-12-19 17:10:45 -0300

    Add funções de validação

--- JS/script.js
+++ JS/script.js
@@ -32,3 +32,53 @@ window.onload = function () {
     txtCep.addEventListener ('blur' , buscaCep);
     
 }
+
+
+
+function cadastrar () {
+    let nome = document.getElementById('txtNome').value;
+    let CPF = document.getElementById ('txtCPF').value;
+    let email = document.getElementById('txtEmail').value;
+    let cel = document.getElementById ('txtTelefone').value;
+    let CEP2 = document.getElementById ('txtCEP').value;
+    let rua = document.getElementById ("txtRua").value;
+    let num = document.getElementById ('txtNum').value;
+    let bairro = document.getElementById ("txtBairro").value;
+    let cidade = document.getElementById ("txtCidade").value;
+    let estado = document.getElementById ("txtEstado").value;
+
+if (nome == '')  {
+        alert ('preencher nome')
+    } else 
+        if (CPF.length < 11) {
+        alert ('CPF inválido')
+    } else
+        if (email == '') {
+            alert ('preencher e-mail')
+    } else 
+        if(cel.length < 11 || cel.length > 13) {
+            alert ('Celular inválido')
+    } else 
+        if (CEP2 == ''){
+            alert ('preencher CEP')
+    } else
+        if (rua == '') {
+            alert ('preencher rua')
+    } else
+        if (bairro == '') {
+            alert ('preencher bairro')
+    } else 
+        if (cidade == '') {
+            alert ('preencher cidade')
+    } else 
+        if (estado == '') {
+            alert ('preencher estado')
+    } else 
+        if (num == ''){
+            alert ('preencher número')
+    }
+    else {
+        alert('Cadastro Efetuado com sucesso!')
+        window.location.reload()}
+    
+}
